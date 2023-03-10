4130c3bf81c11afd262fd59f85ab9482282cc26b
Paul Vidal <u1d4lp@gmail.com>
2023-03-10 17:15:02 -0300

    feat: utils / mnemonics / generate-mnemonics / view-model

---
+++ src/utils/mnemonics/controllers/view-models/generate-mnemonics.view-model.spec.ts
@@ -0,0 +1,14 @@
+import { GenerateMnemonicsViewModel } from './generate-mnemonics.view-model';
+
+describe('GenerateMnemonicsViewModel', () => {
+  it('should create an instance', () => {
+    expect(new GenerateMnemonicsViewModel()).toBeTruthy();
+  });
+
+  it('should have the mnemonic property', () => {
+    const mnemonic = 'mnemonic';
+    const generateMnemonicsViewModel = new GenerateMnemonicsViewModel();
+    generateMnemonicsViewModel.mnemonic = mnemonic;
+    expect(generateMnemonicsViewModel.mnemonic).toEqual(mnemonic);
+  });
+});
