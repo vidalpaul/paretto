0f21f673f9de6b37efe908e82f09f6cc12a6bf3d
Paul Vidal <u1d4lp@gmail.com>
2023-03-10 12:37:47 -0300

    fix: utils / units / provider / + logger

--- src/utils/units/providers/units-util.provider.ts
+++ src/utils/units/providers/units-util.provider.ts
@@ -10,11 +10,15 @@ import {
 
 @Injectable()
 export class UnitsUtilProvider implements UnitsUtilsPort {
+  private readonly logger = new Logger(UnitsUtilProvider.name);
+
   toPlanck(dot: number, precision: 10 | 12): number {
     if (isNaN(dot)) {
+      this.logger.error('UnitsConversionFromNaNFailed');
       throw new UnitsConversionFromNaNFailed();
     }
     if (dot < 0) {
+      this.logger.error('UnitsConversionFromNegativeFailed');
       throw new UnitsConversionFromNegativeFailed();
     }
     return new Big(dot).times(new Big(10).pow(precision)).toNumber();
@@ -22,9 +26,11 @@ export class UnitsUtilProvider implements UnitsUtilsPort {
 
   fromPlanck(planck: number, precision: 10 | 12): number {
     if (isNaN(planck)) {
+      this.logger.error('UnitsConversionFromNaNFailed');
       throw new UnitsConversionFromNaNFailed();
     }
     if (planck < 0) {
+      this.logger.error('UnitsConversionFromNegativeFailed');
       throw new UnitsConversionFromNegativeFailed();
     }
     return new Big(planck).div(new Big(10).pow(precision)).toNumber();
