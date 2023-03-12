7e6e3de89c4a2f4ed1659c4a13aa295e53c338e4
Paul <u1d4lp@gmail.com>
2023-03-12 19:53:10 -0300

    feat: utils / mnemonics / generate / service

---
+++ src/utils/mnemonics/services/generate-mnemonics.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+
+import { GenerateMnemonicsViewModel } from '../controllers/view-models/generate-mnemonics.view-model';
+import { GenerateMnemonicsUsecase } from './usecases/generate-mnemonics.usecase';
+import { GenerateMnemonicsPorts } from '../providers/ports/generate-mnemonics.port';
+
+@Injectable()
+export class GenerateMnemonicsService implements GenerateMnemonicsUsecase {
+  constructor(
+    private readonly generateMnemonicsPorts: GenerateMnemonicsPorts,
+  ) {}
+  /**
+   * Generate a mnemonic phrase
+   * @returns {Promise<GenerateMnemonicsViewModel>}
+   * @memberof GenerateMnemonicsService
+   * @throws {GenerateMnemonicsFailed}
+   */
+  async generateMnemonics(): Promise<GenerateMnemonicsViewModel> {
+    const mnemonics = await this.generateMnemonicsPorts.generateMnemonics();
+    return new GenerateMnemonicsViewModel(mnemonics);
+  }
+}
